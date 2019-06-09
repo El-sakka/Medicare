de78f8d30386283eddf7fc6c46b6ad35b74a0d59
Elsakka <mahmoudelsakka95@gmail.com>
2019-06-09 20:45:03 +0200

    Initial commit

---
+++ app/src/main/java/com/sakkawy/medicare/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sakkawy.medicare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+   // ImageView ivMedicareLogo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_layout);
+
+     //   ivMedicareLogo = findViewById(R.id.iv_medicare_logo);
+
+        //R.drawable.medi_care_logo.png;
+    }
+}
