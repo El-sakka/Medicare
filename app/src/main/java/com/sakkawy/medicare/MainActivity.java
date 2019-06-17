7609b7858e9f8f0b4c502ebb0cc6bb32e20a6cce
mohamed azzam <azzammohamed923@gmail.com>
2019-06-17 20:56:25 +0200

    settings and patient info

--- app/src/main/java/com/sakkawy/medicare/MainActivity.java
+++ app/src/main/java/com/sakkawy/medicare/MainActivity.java
@@ -7,24 +7,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.sakkawy.medicare.View.AddMedicineActivity;
 import com.sakkawy.medicare.View.LoginActivity;
+import com.sakkawy.medicare.View.addEventActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-//    Button btnDone;
+    Button btnDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_main);
 
-//        btnDone = findViewById(R.id.btn_done);
-//        btnDone.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        btnDone = findViewById(R.id.btn_done);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, addEventActivity.class );
+                Intent intent = new Intent(MainActivity.this , AddMedicineActivity.class) ;
+                 startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sakkawy/medicare/View/DisplayPatientInformation.java
@@ -0,0 +1,15 @@
+package com.sakkawy.medicare.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sakkawy.medicare.R;
+
+public class DisplayPatientInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_patient_information);
+    }
+}
